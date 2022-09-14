e2a30957af2600e545df570bf4986642189ce285
Simone Fiorentini <63110203+Simonoob@users.noreply.github.com>
2022-09-14 18:15:06 +0300

    feat: setup DaisyUI

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 function App() {
 	return (
 		<main>
-			<header className="grid items-center grid-flow-row min-h-screen w-screen">
-				<h1 className="text-center">Setup</h1>
+			<header className="grid items-center justify-center grid-flow-row min-h-screen w-screen">
+				<h1 className="btn btn-primary">Setup</h1>
 			</header>
 		</main>
 	)
