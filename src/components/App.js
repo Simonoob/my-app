af261c6c6475e6337d502e339eeac83ee5b55b68
Simone Fiorentini <63110203+Simonoob@users.noreply.github.com>
2022-09-14 18:11:42 +0300

    chore: setup basic project structure

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<main>
+			<header className="grid items-center grid-flow-row min-h-screen w-screen">
+				<h1 className="text-center">Setup</h1>
+			</header>
+		</main>
+	)
+}
+
+export default App
